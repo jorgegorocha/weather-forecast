b56d62c9833e5f509415239c8978eba5cfa8ef93
Jorge Rocha <jorgerocha@iopo0488.lan>
2016-08-07 19:45:39 +0100

    Fixed the jar path

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,6 +1,6 @@
 exports.config = {
     framework: 'jasmine',
-    seleniumServerJar: 'node_modules/selenium-server-standalone-jar/jar/selenium-server-standalone-2.53.0.jar',
+    seleniumServerJar: 'node_modules/selenium-server-standalone-jar/jar/selenium-server-standalone-2.53.1.jar',
     //seleniumAddress: 'http://localhost:4444/wd/hub',
     specs: ['source/tests/protractor/**/*.js']
 }
